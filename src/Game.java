8f155ddb27eaeafaff40586e152c0711e55c34f2
XAVIER TEY <xavierteysh@gmail.com>
2024-03-01 11:37:38 +0800

    wtv

--- src/Game.java
+++ src/Game.java
@@ -3,10 +3,22 @@ public class Game {
     public static void main(String[] args) {
         Deck myDeck = new Deck(Suit.VALUES, Rank.VALUES);
         myDeck.shuffle();
-        while (!myDeck.isEmpty()) {
-            Card dealt = myDeck.dealCard();
-            System.out.println(dealt);
+        Hand myHand = new Hand();
+        System.out.println("Before:");
+        for (int i = 0; i < 3; i++) {
+            myHand.addCard(myDeck.dealCard());
+            System.out.println(myHand.getCard(i));
         }
+        System.out.println("After");
+        myHand.sort();
+        for(int i = 0; i < 3; i++) {
+            System.out.println(myHand.getCard(i));
+        }
+    
+        // while (!myDeck.isEmpty()) {
+        //     Card dealt = myDeck.dealCard();
+        //     System.out.println(dealt);
+        // }
 
     }
 }
