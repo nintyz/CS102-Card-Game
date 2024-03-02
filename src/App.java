81f28010965685fb3695ade114648f2b804ce60b
Aaron <aaronjiteck@gmail.com>
2024-03-02 22:03:56 +0800

    fix path to get fxml file

--- src/App.java
+++ src/App.java
@@ -9,12 +9,12 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        File fxmlFile = new File("D:\\School\\Sem 2\\CS102 Programming Fundamentals\\Project\\CS102-Card-Game\\resources\\match-cards.fxml");
-        // FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/resources/match-cards.fxml"));
-        // Parent root = FXMLLoader.load(getClass().getResource("resources/match-cards.fxml"));
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(fxmlFile.toURI().toURL());
+
+        // Load the FXML(UI) file
+        FXMLLoader loader = new FXMLLoader(new File("resources/match-cards.fxml").toURI().toURL());
         Scene scene = new Scene(loader.load());
+
+        // Set the parameters of the stage(game window)
         stage.setTitle("Matching Game");
         stage.setScene(scene);
         stage.setResizable(false);
